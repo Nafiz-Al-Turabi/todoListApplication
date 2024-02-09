6c26f966fb1fc2a00abb07e3d74f02002a6b7456
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2024-02-09 18:47:55 +0600

    toggle complete added and some small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { RiDeleteBin6Line } from "react-icons/ri";
-import { FaPencilAlt } from "react-icons/fa";
+import { FaPencilAlt, FaRegCheckCircle } from "react-icons/fa";
+import { FaCircleCheck } from "react-icons/fa6";
 import { useState } from 'react';
 
 function App() {
@@ -21,22 +22,37 @@ function App() {
     setTasks(tasks.filter(task => task.id !== id));
   };
 
+  // TODO
+  const updateTask = id => {
+
+  }
+
+  // Mark As Complete Handler
+  const toggleComplete = id => {
+    setTasks(
+      tasks.map(task =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
   return (
     <div className='bg-gradient-to-r to-violet-600 from-violet-400 h-screen'>
       <h1 className='hidden lg:block text-2xl  text-center font-bold uppercase pt-10 mb-5'>Todo List Application</h1>
       <div className='max-w-5xl mx-auto h-screen lg:h-3/4  bg-slate-700 lg:rounded-md shadow-2xl'>
-        <div className='p-6 md:p-4 lg:p-8'>
-          <div className='flex justify-between items-center mb-6 '>
+        <main className='p-6 md:p-4 lg:p-8'>
+          <header className='flex justify-between items-center mb-6 '>
             <div>
-              <p className='text-lg text-gray-200'>Friday, Feb 9</p>
+              <h1 className='text-lg font-bold text-white'>Todo List</h1>
+              <p className='text-xs text-gray-200'>Friday, Feb 9</p>
             </div>
             <div className='md:flex gap-10 text-gray-200'>
               <p className='text-lg'>Total Tasks: {tasks.length} </p>
               <p className='text-lg'>Completed Tasks: 12 </p>
             </div>
-          </div>
+          </header>
           {/*  */}
-          <div className=' w-full lg:w-3/5 space-y-2 md:space-x-1 lg:space-x-2 mb-10'>
+          <section className=' w-full lg:w-3/5 space-y-2 md:space-x-1 lg:space-x-2 mb-10'>
             <input
               type="text"
               onChange={e => setNewTask(e.target.value)}
@@ -52,31 +68,34 @@ function App() {
               <option value="high">High</option>
             </select>
             <button onClick={handleAddTask} className='w-full md:w-24 text-white p-2  font-semibold bg-green-500 rounded uppercase hover:bg-green-400 active:scale-95 duration-300 '>Add task</button>
-          </div>
+          </section>
           {/*  */}
-          <div className='overflow-y-auto h-72 scrollable-container'>
+          <section className='overflow-y-auto h-72 scrollable-container'>
             <div>
               {/* TODO Filter  */}
             </div>
             <div className='space-y-2'>
               {
                 tasks.map(task =>
-                  <div key={task.id} className='text-gray-400 flex justify-between items-center border-y bg-slate-400/10 border-gray-400 p-3 mr-2'>
+                  <div key={task.id} className='text-gray-200 flex justify-between items-center border-y bg-slate-400/10 border-gray-400 p-3 mr-2'>
                     <div className='flex gap-5 items-center '>
-                      <input type="checkbox" />
-                      <p>{task.text} </p>
+                      <button onClick={() => toggleComplete(task.id)}>
+                        {task.completed ? <FaCircleCheck className="text-green-500 text-lg" /> : <FaRegCheckCircle className="text-lg" />}
+                      </button>
+                      {task.completed ? <p className='text-green-500'>{task.text}</p> : <p>{task.text}</p>}
                     </div>
                     <div className='space-x-5'>
+                      {task.completed ? <span className='bg-green-500 text-white px-1 rounded py-0.5 uppercase'>Completed</span> : ''}
                       <span className='text-red-500'>{task.priority}</span>
-                      <button><FaPencilAlt /></button>
-                      <button onClick={()=> deleteTask(task.id)}><RiDeleteBin6Line /></button>
+                      <button onClick={updateTask}><FaPencilAlt /></button>
+                      <button onClick={() => deleteTask(task.id)}><RiDeleteBin6Line /></button>
                     </div>
                   </div>
                 )
               }
             </div>
-          </div>
-        </div>
+          </section>
+        </main>
       </div>
     </div>
   )
